ccfe9bc0068fffe5bfe35ef81e66e2d5fb7ecaa4
jgunter <jgunter@ccri.com>
2023-12-05 08:29:00 -0500

    Day 1 part 2

--- challenge_code/day1.sh
+++ challenge_code/day1.sh
@@ -25,27 +25,43 @@ function part1 {
 
 function part2 {
 
-  convert_string() {
-    # TODO This doesn't work because eightwothree becomes eigh23... not what we want
-    input_string="$1"
-    echo "$input_string" | sed 's/one/1/g; s/two/2/g; s/three/3/g; s/four/4/g; s/five/5/g; s/six/6/g; s/seven/7/g; s/eight/8/g; s/nine/9/g'
-  }
+  declare -A numbers=(
+    ["one"]=1
+    ["two"]=2
+    ["three"]=3
+    ["four"]=4
+    ["five"]=5
+    ["six"]=6
+    ["seven"]=7
+    ["eight"]=8
+    ["nine"]=9
+  )
 
-  # Total count
-  num_total=0
-
-  # Loop through each line in the file
   while IFS= read -r line; do
-    convert_string "$line"
-    converted_string=$(convert_string "$line")
-    echo "$converted_string"
-    numbers=$(echo "$converted_string" | sed 's/[^0-9]*//g')
-    first_number="${numbers:0:1}"
-    last_number="${numbers: -1}"
-    echo "$first_number$last_number"
+    tmp_word=""
+    for ((i=0; i<${#line}; i++)); do
+      tmp_word+=${line:i:1}
+      for number_str in "${!numbers[@]}"; do
+        if [[ $tmp_word == *"$number_str"* ]]; then
+          # Replaces the number string (minus the last letter) with the number.
+          # The last letter stays because letters do not matter in this case,
+          # and the letter may be part of another number.
+          tmp_word=${tmp_word//${number_str%?}/${numbers[$number_str]}}
+        fi
+      done
+    done
+
+    ################### FROM PART 1 ###################
+    # Get only the numbers
+    the_numbers=$(echo "$tmp_word" | sed 's/[^0-9]*//g')
+    # Get the first and last numbers
+    first_number="${the_numbers:0:1}"
+    last_number="${the_numbers: -1}"
+    # Add them to the running total
     num_total=$((num_total + $first_number$last_number))
+    ###################################################
   done <<< "$INPUT"
-  echo "$num_total"
+echo "$num_total"
 }
 
 part2
--- challenge_code/day2.sh
+++ challenge_code/day2.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 source ../helpers/helper_functions.sh
+
+get_input 2
+
+function part1 {
+
+}
+
+part1
